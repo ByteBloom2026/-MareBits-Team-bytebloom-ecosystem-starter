aaacbf786cabd1d6ebcd9921f7e786b08739e550
TasneemAbuNada <tasneemmazen85@gmail.com>
2026-03-10 14:17:48 +0200

    add GetMenteeNameByIdRequest and refactor GetMenteeNameByIdUseCase

--- src/main/kotlin/domain/usecase/GetMenteeNameByIdUseCase.kt
+++ src/main/kotlin/domain/usecase/GetMenteeNameByIdUseCase.kt
@@ -1,13 +1,25 @@
 package domain.usecase
 import data.repository.MenteeRepository
+import domain.usecase.request.GetMenteeNameByIdRequest
+import domain.model.Mentee
 class GetMenteeNameByIdUseCase(
     private val menteeRepository: MenteeRepository
 ) {
-
-     operator fun invoke(id: String): String? {
-        val mentee = menteeRepository.getMenteeById(id)
-        return mentee?.name
+    operator fun invoke(request: GetMenteeNameByIdRequest):  Result<String?>{
+        return menteeRepository
+            .getMenteeById(request.menteeId)
+            .fold(
+                onSuccess = ::toMenteeNameResult,
+                onFailure = ::toFailureResult
+            )
 
     }
-
+    private fun toMenteeNameResult(mentee: Mentee?): Result<String?> {
+        return Result.success(mentee?.name)
+    }
+    private fun toFailureResult(error: Throwable): Result<String?> {
+        return Result.failure(
+            Exception("Failed to fetch mentee name: ${error.message}")
+        )
+    }
 }
