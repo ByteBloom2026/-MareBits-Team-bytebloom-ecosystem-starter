b69e2e137226bfc7cdaaf7394c2027ef23d19aec
TasneemAbuNada <tasneemmazen85@gmail.com>
2026-03-25 20:58:46 +0200

    update domain exceptions and remove data related stuff

--- src/main/kotlin/domain/model/exception/exceptions.kt
+++ src/main/kotlin/domain/model/exception/exceptions.kt
@@ -1,39 +1,39 @@
 package domain.model.exception
 
-sealed class SearchTeamException(string: String) : Throwable() {
+sealed class SearchTeamException(message: String) : Exception(message){
     class EmptyTeamNameException: SearchTeamException("Team name cannot be empty")
     class InvalidTeamNameLengthException: SearchTeamException("Team name length is invalid")
     class EmptyTeamIdExcpection :SearchTeamException("The team id cannect be empty")
 }
-sealed class MenteeException (string: String): Throwable(){
+sealed class MenteeException (message: String): Exception(message){
     class EmptyMenteeNameException: MenteeException("Mentee name cannot be empty")
     class InvalidMenteeNameException :MenteeException(" The mentee name is invalid ")
 }
-sealed class AttendanceException(string: String): Throwable(){
+sealed class AttendanceException(message: String): Exception(message){
     class EmptyAttendanceWeeksException : AttendanceException("Attendance weeks cannot be empty")
+    class InvalidAttendanceStateException : AttendanceException("Invalid attendance state")
 }
-sealed class SubmissionException(string: String): Throwable(){
+sealed class SubmissionException(message: String): Exception(message){
     class InvalidSubmissionTypeException : SubmissionException("The submission type is invalid ")
     class EmptySumissionTypeException : SubmissionException("The submission type cannot is empty ")
     class InvalidSumbmissionIdException : SubmissionException("The submission Id is invalid")
     class EmptySubmissionIdException:SubmissionException("The submission Id  cannot is empty")
 }
-sealed class ProjectException(string: String): Throwable(){
+sealed class ProjectException(message: String): Exception(message){
     class EmptyProjectNameException : ProjectException("project name cannot be empty")
     class InvalidProjectNameLengthException : ProjectException("Project length name is invalid")
     class InvalidProjectIdException : ProjectException("The project id is invalid ")
 }
-sealed class ScoreException(string: String): Throwable(){
+sealed class ScoreException(message: String): Exception(message){
     class InvalidScoreException : ScoreException("Score is invalid (NaN or Infinite)")
     class ScoreOutOfRangeException(val min : Double,val max : Double) : ScoreException("the score is out of range [$min,$max]")
 }
-sealed class QueryException(string: String): Throwable(){
+sealed class QueryException(message: String): Exception(message){
     class EmptyQueryException : QueryException("Search query cannot be empty")
     class InvalidQueryException :QueryException("Search query is invalid")
 }
 class NotCapitalizedNameException: Exception("Name must start with a letter")
 class NameContainsInvalidCharactersException :Exception("Name must contain only letters")
 sealed class DataAccessException(message: String) : Exception(message) {
-    class InvalidDataSourceException : DataAccessException("Invalid data source")
-    class InvalidDataFormatException : DataAccessException("Invalid data format")
+    class DataUnavailableException : DataAccessException("Data is unavailable")
 }
