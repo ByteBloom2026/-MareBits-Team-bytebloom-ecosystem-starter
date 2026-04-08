e5110da88daac9ace2b6095007c0aa396f3b3584
IbtisamSafi <safiibtisam891@gmail.com>
2026-04-09 00:48:55 +0300

    Fix: Resolved errors

--- src/test/kotlin/di/testModule.kt
+++ src/test/kotlin/di/testModule.kt
@@ -4,14 +4,15 @@ import data.repository.PerformanceRepository
 import data.repository.TeamRepository
 import io.mockk.mockk
 import org.koin.dsl.module
+import fakeRepository.*
 
 class testModule {
     val teamRepoTest=mockk<TeamRepository>()
     val performance=mockk<PerformanceRepository>()
 
     val repositorytestModule = module {
-        single { teamRepoTest }
-        single { Performance }
+        single { FakeTeamRepository() }
+        single { FakePerformanceRepository() }
     }
     val useCaseTestModule =module {
         single { :: GenerateCrossTeamPreformanceReportUseCase }
