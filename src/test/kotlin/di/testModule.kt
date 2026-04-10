f2558c8a392f99be265297e371b297bb21bf5241
TasneemAbuNada <tasneemmazen85@gmail.com>
2026-04-10 16:52:41 +0300

    add testModule to combine repository and use case test modules

--- src/test/kotlin/di/testModule.kt
+++ src/test/kotlin/di/testModule.kt
@@ -1,21 +1,6 @@
 package di
-import domain.usecase.*
-import data.repository.PerformanceRepository
-import data.repository.TeamRepository
-import io.mockk.mockk
-import org.koin.dsl.module
-import fakeRepository.*
 
-class testModule {
-    val teamRepoTest=mockk<TeamRepository>()
-    val performance=mockk<PerformanceRepository>()
-
-    val repositorytestModule = module {
-        single { FakeTeamRepository() }
-        single { FakePerformanceRepository() }
-    }
-    val useCaseTestModule =module {
-        single { :: GenerateCrossTeamPreformanceReportUseCase }
-    }
-}
-
+val testModule = listOf(
+    repositoryTestModule,
+    UseCaseTestModule
+)
