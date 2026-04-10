094d175c7ce062fdc58fc5041a5dcc99833f17b0
TasneemAbuNada <tasneemmazen85@gmail.com>
2026-04-10 19:56:50 +0300

    add unit tests for attendance percentage use case using Koin and fake repository

--- src/test/kotlin/domain/usecase/CalculateOverallAttendancePercentageUseCaseTest.kt
+++ src/test/kotlin/domain/usecase/CalculateOverallAttendancePercentageUseCaseTest.kt
@@ -1,24 +1,34 @@
 package domain.usecase
 import com.google.common.truth.Truth.assertThat
+import di.testModule
 import domain.usecase.request.CalculateOverallAttendancePercentageRequest
 import fakeRepository.FakeAttendanceRepository
 import org.junit.jupiter.api.Test
-class CalculateOverallAttendancePercentageUseCaseTest {
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.test.KoinTest
+import org.koin.test.inject
+class CalculateOverallAttendancePercentageUseCaseTest : KoinTest{
+    private val calculateOverallAttendancePercentageUseCase: CalculateOverallAttendancePercentageUseCase by inject()
+    @BeforeEach
+    fun setUp() {
+        startKoin { modules(testModule) }
+    }
+    @AfterEach
+    fun tearDown() { stopKoin() }
     @Test
-    fun shouldCalculateOverallAttendancePercentage() {
-        val repository = FakeAttendanceRepository()
-        val useCase = CalculateOverallAttendancePercentageUseCase(repository)
+    fun `shouldCalculateOverallAttendancePercentage`() {
         val request = CalculateOverallAttendancePercentageRequest("m001")
-        val result = useCase(request)
+        val result = calculateOverallAttendancePercentageUseCase(request)
         assertThat(result.isSuccess).isTrue()
         assertThat(result.getOrNull()).isEqualTo(66.67)
     }
     @Test
-    fun shouldCalculateAttendancePercentageWithLateAsHalfPoint() {
-        val repository = FakeAttendanceRepository()
-        val useCase = CalculateOverallAttendancePercentageUseCase(repository)
+    fun `shouldCalculateAttendancePercentageWithLateAsHalfPoint`() {
         val request = CalculateOverallAttendancePercentageRequest("m004")
-        val result = useCase(request)
+        val result = calculateOverallAttendancePercentageUseCase(request)
         assertThat(result.isSuccess).isTrue()
         assertThat(result.getOrNull()).isEqualTo(50.0)
     }
