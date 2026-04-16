579765cd9c8f6ad14905116c6863469b593941f3
TasneemAbuNada <tasneemmazen85@gmail.com>
2026-04-16 19:34:16 +0300

    update GetTopPerformingMenteesBySubmissionTypeUseCase tests and add new test case

--- src/test/kotlin/domain/usecase/GetTopPerformingMenteesBySubmissionTypeUseCaseTest.kt
+++ src/test/kotlin/domain/usecase/GetTopPerformingMenteesBySubmissionTypeUseCaseTest.kt
@@ -10,21 +10,55 @@ import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.test.KoinTest
 import org.koin.test.inject
-class GetTopPerformingMenteesBySubmissionTypeUseCaseTest : KoinTest{
+class GetTopPerformingMenteesBySubmissionTypeUseCaseTest : KoinTest {
     private val getTopPerformingMenteesBySubmissionTypeUseCase: GetTopPerformingMenteesBySubmissionTypeUseCase by inject()
+
     @BeforeEach
-    fun setUp() { startKoin { modules(testModule) } }
+    fun setUp() {
+        startKoin { modules(testModule) }
+    }
+
     @AfterEach
-    fun tearDown() { stopKoin() }
+    fun tearDown() {
+        stopKoin()
+    }
 
     @Test
-    fun `shouldReturnTopPerformingMenteeForTaskSubmissionType`() {
-
+    fun `should Return Top Performing Mentee For Task Submission Type`() {
+        //given
         val request = GetTopPerformingMenteesBySubmissionTypeRequest(
             PerformanceSubmission.SubmissionType.TASK
         )
-        val result =getTopPerformingMenteesBySubmissionTypeUseCase (request)
-        assertThat(result.isSuccess).isTrue()
-        assertThat(result.getOrNull()?.map { it.id }).containsExactly("m001")
+        //when
+        val result = getTopPerformingMenteesBySubmissionTypeUseCase(request)
+        val returnedMenteeIds = result.getOrNull()?.map { it.id }
+        //then
+        assertThat(returnedMenteeIds).containsExactly("m001")
+    }
+
+    @Test
+    fun `should return m003 as top performing mentee for book club submission type`() {
+        //given
+        val request = GetTopPerformingMenteesBySubmissionTypeRequest(
+            PerformanceSubmission.SubmissionType.BOOK_CLUB
+        )
+        //when
+        val result = getTopPerformingMenteesBySubmissionTypeUseCase(request)
+        val returnedMenteeIds = result.getOrNull()?.map { it.id }
+        //then
+        assertThat(returnedMenteeIds).containsExactly("m003")
+    }
+
+    @Test
+    fun `should return m003 as top performing mentee for workshop submission type`() {
+       //given
+        val request = GetTopPerformingMenteesBySubmissionTypeRequest(
+            PerformanceSubmission.SubmissionType.WORKSHOP)
+        //when
+        val result = getTopPerformingMenteesBySubmissionTypeUseCase(request)
+        val returnedMenteeIds = result.getOrNull()?.map { it.id }
+        //then
+        assertThat(returnedMenteeIds).containsExactly("m003")
+
     }
 }
