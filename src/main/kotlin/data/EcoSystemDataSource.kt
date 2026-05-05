05acbb6a967783e37046f0c2d1d7da5398b01aa7
TasneemAbuNada <tasneemmazen85@gmail.com>
2026-05-05 13:32:25 +0300

    refactor: convert EcoSystemDataSource interface functions to suspend

--- src/main/kotlin/data/EcoSystemDataSource.kt
+++ src/main/kotlin/data/EcoSystemDataSource.kt
@@ -1,16 +1,16 @@
 package data
 import data.datasource.model.*
 interface EcoSystemDataSource {
-    fun getMentees(): Result<List<MenteeRow>>
-    fun getMenteeById(id: String): Result<MenteeRow?>
-    fun getMenteesByTeamId(teamId: String): Result<List<MenteeRow>>
-    fun getTeams(): Result<List<TeamRow>>
-    fun getTeamById(teamId: String): Result<TeamRow?>
-    fun getPerformances(): Result<List<PerformanceRow>>
-    fun getPerformanceByMenteeId(menteeId: String): Result<List<PerformanceRow>>
-    fun getProjects(): Result<List<ProjectRow>>
-    fun getProjectByTeamId(teamId: String): Result<ProjectRow?>
-    fun getAttendances(): Result<List<AttendanceRow>>
-    fun getAttendanceByMenteeId(menteeId: String): Result<AttendanceRow?>
-    fun getPerformanceByTeamId(teamId: String): Result<List<PerformanceRow>>
+    suspend fun getMentees(): Result<List<MenteeRow>>
+    suspend fun getMenteeById(id: String): Result<MenteeRow?>
+    suspend fun getMenteesByTeamId(teamId: String): Result<List<MenteeRow>>
+    suspend fun getTeams(): Result<List<TeamRow>>
+    suspend fun getTeamById(teamId: String): Result<TeamRow?>
+    suspend fun getPerformances(): Result<List<PerformanceRow>>
+    suspend fun getPerformanceByMenteeId(menteeId: String): Result<List<PerformanceRow>>
+    suspend fun getProjects(): Result<List<ProjectRow>>
+    suspend fun getProjectByTeamId(teamId: String): Result<ProjectRow?>
+    suspend fun getAttendances(): Result<List<AttendanceRow>>
+    suspend fun getAttendanceByMenteeId(menteeId: String): Result<AttendanceRow?>
+    suspend fun getPerformanceByTeamId(teamId: String): Result<List<PerformanceRow>>
 }
