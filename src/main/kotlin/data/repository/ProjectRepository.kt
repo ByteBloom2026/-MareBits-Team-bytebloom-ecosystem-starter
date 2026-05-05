1f4feaa626fd1ac6eeb9368b337104a29aa9b99b
TasneemAbuNada <tasneemmazen85@gmail.com>
2026-05-05 13:35:26 +0300

    refactor: update ProjectRepository interface to use suspend functions

--- src/main/kotlin/data/repository/ProjectRepository.kt
+++ src/main/kotlin/data/repository/ProjectRepository.kt
@@ -1,6 +1,6 @@
 package data.repository
 import domain.model.Project
 interface ProjectRepository {
-    fun getAllProjects(): Result<List<Project>>
-    fun getProjectByTeamId(teamId: String): Result<Project?>
+    suspend fun getAllProjects(): Result<List<Project>>
+    suspend fun getProjectByTeamId(teamId: String): Result<Project?>
 }
