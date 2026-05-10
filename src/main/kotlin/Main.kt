0202532262f2725cb967c4e7e629aafe838669c0
IbtisamSafi <safiibtisam891@gmail.com>
2026-05-10 12:53:46 +0300

    "The UseCase was invoked in the Main function."

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,40 @@
-import java.io.File
-import data.datasource.CsvEcosystemDataSource
-import domain.usecase.*
-import data.repository.*
-import domain.model.Team
-import domain.usecase.request.GetMenteeNameByIdRequest
-import domain.usecase.request.GetMenteeNamesByTeamNameRequest
-import domain.usecase.request.SearchTeamsByNameRequest
 import kotlinx.coroutines.runBlocking
+import domain.usecase.GetMenteeNamesByTeamNameUseCase
+import data.repository.*
+import data.datasource.CsvEcosystemDataSource
+import domain.usecase.request.GetMenteeNamesByTeamNameRequest
+import java.io.File
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
-fun main() = runBlocking{
-   val   teamFile = File("src/main/resources/teams.csv")
-    val  menteeFile = File("src/main/resources/mentees.csv")
-    val teamRepository = TeamRepositoryImpl(teamFile)
-    val menteeRepository = MenteeRepositoryImpl(menteeFile)
-
-    val getMenteeNamesByTeamNameUseCase=
-        GetMenteeNamesByTeamNameUseCase(teamRepository,menteeRepository)
-        .invoke(GetMenteeNamesByTeamNameRequest(""))
+fun main ()=runBlocking {
+    val csvDataSource = CsvEcosystemDataSource(
+        File("src/main/resources/mentees.csv"),
+        File("src/main/resources/teams.csv"),
+        File("src/main/resources/performance.csv"),
+        File("src/main/resources/projects.csv"),
+        File("src/main/resources/attendance.csv")
+    )
+    val menterepo = MenteeRepositoryImpl(csvDataSource)
+    val Teamrepo = TeamRepositoryImpl(csvDataSource)
+    val getMenteeNamesByTeamNameUseCase =
+        GetMenteeNamesByTeamNameUseCase(Teamrepo, menterepo)
+    val getMenteeNamesByTeamNameRequest = GetMenteeNamesByTeamNameRequest(teamName = " ")
 
 
     getMenteeNamesByTeamNameUseCase
+        .invoke(getMenteeNamesByTeamNameRequest)
+        .onSuccess { namesFlow ->
+            namesFlow
+                .onEach { name ->
+                }
+                .launchIn(this)
+
+        }
+
 
 }
+
 
 
 
